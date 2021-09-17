93e1accf8c6a339c3bbbe59b6fc41d11774a7517
JungHyoMoon <dsinhome70@gmail.com>
2021-09-17 14:15:09 +0900

    apollo is great. todo: what is cache?

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import client from "./apollo";
+import { ApolloProvider } from "@apollo/react-hooks";
+
+// CRA처럼, 이미 대부분의 것들을 준비해둔 'apollo-boost'.
+// client 설정만 해주면 간단하게 사용할 수 있음. (client: 데이터 요청할 url?)
 
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById("root")
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById("root")
 );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,29 @@
 import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
 
-const Home = () => <div>Home</div>;
+const GET_MOVIES = gql`
+    {
+        movies {
+            id
+            title
+            medium_cover_image
+        }
+    }
+`;
+
+const Home = () => {
+    const { loading, error, data } = useQuery(GET_MOVIES);
+    console.log(data);
+    return (
+        <div>
+            {loading ? (
+                <h1>loading...</h1>
+            ) : (
+                data.movies.map((movie) => <h1>{movie.title}</h1>)
+            )}
+        </div>
+    );
+};
 
 export default Home;
